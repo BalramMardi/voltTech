ca4b8b87b9d18c711c93f44fd18e75a297631200
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2024-04-23 00:18:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 //databse config
 connectDB();
 
-const __filename = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //rest object
